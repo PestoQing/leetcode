93423d60a2edd571d04e9f69213b89a097cc7fdd
PestoQing <PestoQing@users.noreply.github.com>
2026-09-18 17:25:56 +0800

    977双指针

--- 数组/977有序数组的平方.py
+++ 数组/977有序数组的平方.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         # for i in range(len(nums)):
@@ -6,9 +7,15 @@ class Solution:
         # return nums
         head = 0
         tail = len(nums)-1
-        while head <=tail:
-            if abs(nums[head])<abs(nums[tail]):
-                res
+        res = [0]*len(nums)
+        for i in reversed(range(len(nums))):
+            if abs(nums[head])>abs(nums[tail]):
+                res [i] = nums[head] *nums[head]
+                head+=1
+            else:
+                res[i] = nums[tail] * nums[tail]
+                tail -=1
+        return res
 
 nums = [-4,-1,0,3,10]
 print(Solution().sortedSquares(nums))
