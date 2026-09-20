5d393c8d524558ce4c8ec8afaf188ffa311800f8
PestoQing <PestoQing@users.noreply.github.com>
2026-09-20 10:20:46 +0800

    1

--- 数组/209.长度最小的子数组.py
+++ 数组/209.长度最小的子数组.py
@@ -3,20 +3,23 @@ class Solution:
     def minSubArrayLen(self, target: int, nums: list[int]) -> int:
         start,end = 0,0
         res = float('inf')
-        def list_sum(start,end):
-            sum = 0
-            for i in range(start,end+1):
-                sum+=nums[i]
-            return sum
-        while end <= len(nums)-1:
-            while list_sum(start,end) <target:
-                end+=1
-            while list_sum(start,end)>=target:
-                start+=1
-            res = min(end - start +1 ,res) 
-            
-            
+        sum_=nums[0]
+        while end<=len(nums)-1:
 
+            while sum_<target :
+                end+=1
+                if end>len(nums)-1:
+                    if res == float('inf'):
+                        return 0
+                    return res
+                sum_ += nums[end]
+            # res=min(res,end-start+1)
+            while sum_ >= target:
+                sum_ -= nums[start]
+                start +=1
+            res=min(res,end-start+1+1)
+        if res == float('inf'):
+            return 0
         return res
 
 target = 7
