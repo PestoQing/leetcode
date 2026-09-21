1665de7f3127f6172990ee934e39d1a974cb4b4b
PestoQing <PestoQing@users.noreply.github.com>
2026-09-21 18:04:31 +0800

    1

--- 数组/59.螺旋矩阵II.py
+++ 数组/59.螺旋矩阵II.py
@@ -4,6 +4,7 @@ class Solution:
         res=[[0]*n for _ in range(n)]
         count = 1
         layer = 0
+        tmp= n
         if n % 2 == 1:
             res[(n-1)//2][(n-1)//2]=n*n
             while n >1:
@@ -11,18 +12,36 @@ class Solution:
                     res[layer][i+layer] = count
                     count+=1
                 for i in range(n-1):
-                    res[i+layer][n-1] = count
+                    res[i+layer][tmp-1 -layer] = count
                     count+=1
                 for i in range(n-1):
-                    res[n-1+layer][n-1-i-layer] = count
+                    res[tmp-1-layer][tmp-1-i-layer] = count
                     count+=1
                 for i in range(n-1):
-                    res[n-1-i-layer][layer] = count
+                    res[tmp-1-i-layer][layer] = count
                     count+=1
                 n=n-2
                 layer+=1
             
+        else:
+
+   
+            while n >1:
+                for i in range(n-1):
+                    res[layer][i+layer] = count
+                    count+=1
+                for i in range(n-1):
+                    res[i+layer][tmp-1 -layer] = count
+                    count+=1
+                for i in range(n-1):
+                    res[tmp-1-layer][tmp-1-i-layer] = count
+                    count+=1
+                for i in range(n-1):
+                    res[tmp-1-i-layer][layer] = count
+                    count+=1
+                n=n-2
+                layer+=1
 
         return res
 
-print(Solution().generateMatrix(5))
+print(Solution().generateMatrix(4))
